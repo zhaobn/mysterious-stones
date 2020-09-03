0df5e9e8bda6b122a1556addce1ab9b7f0fa2670
bonan <zbn.dale@gmail.com>
2020-09-03 23:43:38 +0100

    Refactor pcfg, calc priors

--- models/pcfg.R
+++ models/pcfg.R
@@ -1,28 +1,8 @@
 
-#### Glabal variables ####
-# configs
-feature_setting<-list()
-feature_setting[['edges']]<-seq(3,8)
-feature_setting[['shades']]<-seq(4)
+options("scipen" = 10)
+options()$scipen
 
-# defined according to configs
-all_objects<-vector()
-for (e in feature_setting$edges) {
-  for (s in feature_setting$shades) {
-    all_objects<-c(all_objects, paste(c(e,s), collapse=''))
-  }
-}
-
-dist<-list()
-for (o in all_objects) dist[[o]]<-0
-
-
-all_data_str<-vector()
-for (a in all_objects) {
-  for (r in all_objects) {
-    all_data_str<-c(all_data_str, paste(c(a,r),collapse=',')) 
-  }
-}
+source('./shared.R')
 
 ################################################################
 ##### PCFG ####
@@ -57,70 +37,17 @@ generate_hypo<-function() {
   return(paste0("list(cause='",cause,"',effect='",effect,"')"))
 }
 
-#### Evaluation ####
-edges<-function(x) floor(x/10)
-shades<-function(x) x%%10
-and<-function(x, y) {
-  if (missing(x)) x<-NA
-  if (missing(y)) y<-NA
-  return(x&y)
-}
-
-normalize<-function(raw) {
-  if (typeof(raw)=='list') {
-    sum<-Reduce('+', raw)
-    if (sum==0) return(raw) else {
-      for (r in names(raw)) raw[[r]]<-raw[[r]]/sum; return(raw)
-    }
-  } else return(raw/sum(raw))
-}
-
-causal_mechanism<-function(hypo, data) {
-  # Format input data
-  if (typeof(data)!='list') {
-    obs<-strsplit(data, ',')[[1]]
-    data<-list(agent=obs[1], recipient=obs[2], result=obs[3])
-  }
-  
-  hypo<-gsub('A', data$agent, hypo)
-  hypo<-gsub('R', data$recipient, hypo)
-  
-  input<-eval(parse(text=hypo))
-  # Take care of empty ones
-  if (input$cause==''|input$effect=='') {
-    dist[[as.character(data$recipient)]]<-1
-  } else {
-    # Check cause conditions
-    cause_cond<-eval(parse(text=input$cause))
-    # Default to no change
-    if (is.na(cause_cond)|!cause_cond) {
-      dist[[as.character(data$recipient)]]<-1
-    } else {
-      na_effect<-F
-      # Check for all possible result object
-      for (d in names(dist)) {
-        effect_text<-input$effect
-        effect_text<-gsub('M', d, effect_text)
-        if (is.na(eval(parse(text=effect_text)))) {
-          na_effect<-T; break;
-        } else {
-          dist[[d]]<-as.numeric(eval(parse(text=effect_text)))
-        }
-      }
-      if (na_effect) dist[[as.character(data$recipient)]]<-1 else dist<-normalize(dist)
-    }
-  }
-  return(dist)
-}
-
 ################################################################
 # Group causally-equivalent hypothesis ####
+# hypos<-list()
+# for (i in seq(20000)) hypos[[i]]<-generate_hypo()
+# hypos<-unique(hypos)
+
 load('hypos.Rdata')
-some_hypos<-hypos[seq(10)]
+some_hypos<-hypos[seq(100)]
 data_strs<-all_data_str[seq(3)]
 
-list_info<-data.frame(current=0)
-
+#list_info<-data.frame(current=0)
 results<-list()
 for (hi in 1:length(some_hypos)) {
   h<-some_hypos[[hi]]
@@ -130,8 +57,9 @@ for (hi in 1:length(some_hypos)) {
     names(x)<-paste0(d, ',', names(x))
     results[[h]]<-c(results[[h]], x)
   }
-  list_info$current<-hi
-  write.csv(list_info, 'list_info.csv')
+  # # logging  
+  # list_info$current<-hi
+  # write.csv(list_info, 'list_info.csv')
 }
 
 save(results, file='results_raw.Rdata')
@@ -155,10 +83,44 @@ for (i in 2:length(ut)) {
   # save up
   df<-rbind(df, group_hypos(i, results))
   save(df, file='hypos_grouped.Rdata')
-  
 }
 
 ################################################################
+# Generative prior
+library(stringr)
+
+pcfg_prior<-function(hypo) {
+  get_prior<-function(x) {
+    # count ands
+    n_and<-str_count(x, 'and')
+    # count ==s
+    n_eq<-str_count(x, '==')
+    # count sub sentence
+    x<-gsub('and\\(', '', x)
+    x<-gsub(' ', '', x)
+    ss<-strsplit(x, ',')[[1]]
+    n_drawn<-length(ss)
+    n_neq<-n_drawn-n_eq
+    # count relative picks
+    rels<-0
+    for (s in ss) {
+      obj<-strsplit(s, '==|!=|>|<')[[1]][2]
+      if (nchar(obj)>5) rels<-rels+1
+    }
+    vals<-n_drawn-rels
+    
+    return((1/3)^n_and*0.625^n_eq*0.124^n_neq*0.5^rels*0.2^vals)
+  }
+  h<-eval(parse(text=hypo))
+  return(get_prior(h$cause)*get_prior(h$effect))
+}
+hypos_grouped$prior_raw<-mapply(pcfg_prior, hypos_grouped$shortest)
+hypos_grouped$prior<-normalize(hypos_grouped$prior_raw)
+save(hypos_grouped, file='../data/hypos_grouped.Rdata')
+
+
+
+
 
 
 
---
+++ models/shared.R
@@ -0,0 +1,97 @@
+
+#### Glabal variables ####
+# configs
+feature_setting<-list()
+feature_setting[['edges']]<-seq(3,8)
+feature_setting[['shades']]<-seq(4)
+
+# defined according to configs
+all_objects<-vector()
+for (e in feature_setting$edges) {
+  for (s in feature_setting$shades) {
+    all_objects<-c(all_objects, paste(c(e,s), collapse=''))
+  }
+}
+
+init_dist<-function() {
+  dist<-list()
+  for (o in all_objects) dist[[o]]<-0
+  return(dist)
+}
+dist<-init_dist()
+
+
+all_data_str<-vector()
+for (a in all_objects) {
+  for (r in all_objects) {
+    all_data_str<-c(all_data_str, paste(c(a,r),collapse=',')) 
+  }
+}
+
+#### Evaluation ####
+edges<-function(x) floor(x/10)
+shades<-function(x) x%%10
+and<-function(x, y) {
+  if (missing(x)) x<-NA
+  if (missing(y)) y<-NA
+  return(x&y)
+}
+
+normalize<-function(raw) {
+  if (typeof(raw)=='list') {
+    sum<-Reduce('+', raw)
+    if (sum==0) return(raw) else {
+      for (r in names(raw)) raw[[r]]<-raw[[r]]/sum; return(raw)
+    }
+  } else return(raw/sum(raw))
+}
+
+listify_data<-function(data_str) {
+  if (typeof(data_str)=='list') return(data_str) else {
+    obs<-strsplit(data_str, ',')[[1]]
+    return(list(agent=obs[1], recipient=obs[2], result=obs[3]))
+  }
+}
+
+causal_mechanism<-function(hypo, data) {
+  data<-listify_data(data)
+  
+  hypo<-gsub('A', data$agent, hypo)
+  hypo<-gsub('R', data$recipient, hypo)
+  
+  input<-eval(parse(text=hypo))
+  # Take care of empty ones
+  if (input$cause==''|input$effect=='') {
+    dist[[as.character(data$recipient)]]<-1
+  } else {
+    # Check cause conditions
+    cause_cond<-eval(parse(text=input$cause))
+    # Default to no change
+    if (is.na(cause_cond)|!cause_cond) {
+      dist[[as.character(data$recipient)]]<-1
+    } else {
+      na_effect<-F
+      # Check for all possible result object
+      for (d in names(dist)) {
+        effect_text<-input$effect
+        effect_text<-gsub('M', d, effect_text)
+        if (is.na(eval(parse(text=effect_text)))) {
+          na_effect<-T; break;
+        } else {
+          dist[[d]]<-as.numeric(eval(parse(text=effect_text)))
+        }
+      }
+      if (na_effect) dist[[as.character(data$recipient)]]<-1 else dist<-normalize(dist)
+    }
+  }
+  return(dist)
+}
+
+get_likeli<-function(hypo, data) {
+  data<-listify_data(data)
+  return(causal_mechanism(hypo, data)[[as.character(data$result)]])
+}
+get_likeli(hypo, data)
+
+
+

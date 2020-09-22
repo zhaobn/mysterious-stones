b7ba94a786e8663a65cb02ea77054df68bd92ca7
bonan <zbn.dale@gmail.com>
2020-09-22 13:36:25 +0100

    Gibbs: sample from multiple new functions rather one 1

--- models/gibbs.R
+++ models/gibbs.R
@@ -3,6 +3,9 @@ source('shared.R')
 tasks<-read.csv('../data/pilot_setup.csv')
 load('../data/effects_grouped.Rdata') # hypothesis
 
+find_func_ref<-function(func, func_ref) {
+  
+}
 
 run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_source=df.effects.posts) {
   task_obs<-tasks%>%filter(group==cond&phase=='learn')%>%select(agent, recipient, result)
@@ -21,6 +24,9 @@ run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_
   state<-rep('c1', nobs)
   func_refs[['c1']]<-sample(hypos$hypo, 1, prob=hypos$prior)
   
+  # Pre-calculated values
+  join_new<-alpha/(nobs-1+alpha)
+  
   # # Debugging tests
   # state=c(paste0('c', seq(5)), 'c2')
   # for (i in 1:5) {
@@ -36,26 +42,33 @@ run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_
   while (n<limit) {
     # sample 1 obs
     to_update<-sample(seq(nobs), 1) # index
-    # sample a function for this obs wrt posterior
-    post_col<-paste0('post_',to_update)
-    funcs_pool<-hypos$hypo[which(hypos[,post_col]>0)]
-    funcs_post<-hypos[which(hypos[,post_col]>0), post_col]
-    new_func<-sample(funcs_pool, 1, prob=funcs_post)
-    # whether the sampled func belongs to an existing category
-    checks<-sapply(func_refs, function(x) x==new_func)
-    if (T %in% checks) {
-      cat<-names(func_refs)[which(checks)]
-    } else {
-      cat<-paste0('c', length(func_refs)+1)
-      func_refs[[cat]]<-new_func
-    }
-    # Check for existing categories
-    propto<-list()
-    other_idx<-setdiff(seq(nobs), to_update)
-    
     obs_to_update<-as.list(task_obs[to_update,])
     obs_feats<-read_feature(obs_to_update, grouping) 
     
+    cat_feat<-Map('+', obs_feats, init_feat_dist(beta))
+    self_resemblance<-sum(unlist(cat_feat)*unlist(obs_feats))/sum(unlist(cat_feat))
+    
+    # sample new function(s) for this obs from conditional probablity
+    post_col<-paste0('post_',to_update)
+    funcs_pool<-hypos$hypo[which(hypos[,post_col]>0)]
+    funcs_post<-hypos[which(hypos[,post_col]>0), post_col]
+    
+    new_funcs<-sample(funcs_pool, 3, prob=funcs_post)
+    new_cats<-c()
+    # whether the sampled func belongs to an existing category
+    for (f in unique(new_funcs)) {
+      checks<-sapply(func_refs, function(x) x==f)
+      if (T %in% checks) {
+        cat<-names(func_refs)[which(checks)]
+      } else {
+        cat<-paste0('c', length(func_refs)+1)
+        func_refs[[cat]]<-f
+      }
+      new_cats<-c(new_cats, cat)
+    }
+
+    # Check for existing categories
+    propto<-list()
     for (s in unique(state)) {
       #ob_indx<-other_idx[which(state==s)]
       ob_indx<-which(state==s)
@@ -71,14 +84,13 @@ run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_
       propto[[s]]<-join_this*resemblance*likeli
     }
     # Or assign a new one
-    if (!(cat %in% state)) {
-      join_new<-alpha/(nobs-1+alpha)
-      cat_feat<-Map('+', obs_feats, init_feat_dist(beta))
-      self_resemblance<-sum(unlist(cat_feat)*unlist(obs_feats))/sum(unlist(cat_feat))
-      post_likeli<-get_likeli(new_func, obs_to_update)
-      propto[[cat]]<-join_new*self_resemblance*post_likeli
-    }
     
+    for (cat in new_cats) {
+      if (!(cat %in% state)) {
+        post_likeli<-get_likeli(func_refs[[cat]], obs_to_update)
+        propto[[cat]]<-join_new*self_resemblance*post_likeli
+      } 
+    }
     # Sample new category
     state[to_update]<-sample(names(propto), 1, prob=unlist(propto))
     # ll<-1

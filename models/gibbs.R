1c75f2726bb32bf15fc81eedd9fb64f590c4dd55
bonan <zbn.dale@gmail.com>
2020-09-22 21:09:15 +0100

    Gibbs: choose state-to-update wisely

--- models/gibbs.R
+++ models/gibbs.R
@@ -3,16 +3,24 @@ source('shared.R')
 tasks<-read.csv('../data/pilot_setup.csv')
 load('../data/effects_grouped.Rdata') # hypothesis
 
-find_func_ref<-function(func, func_ref) {
-  
-}
-
 run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_source=df.effects.posts) {
+  # Task setup
   task_obs<-tasks%>%filter(group==cond&phase=='learn')%>%select(agent, recipient, result)
-  nobs<-nrow(task_obs)
-  
   hypos<-hypo_source%>%filter(group==cond)
-  # non_empty<-hypos%>%filter(post_1>0&post_2>0&post_3>0&post_4>0&post_5>0&post_6>0)
+  #non_empty<-hypos%>%filter(post_1>0&post_2>0&post_3>0&post_4>0&post_5>0&post_6>0)
+
+  # Pre-calculated values
+  nobs<-nrow(task_obs)
+  join_new<-alpha/(nobs-1+alpha)
+  feats<-list()
+  for (i in seq(nobs)) feats[[i]]<-read_feature(as.list(task_obs[i,]), grouping)
+  # Helper function that calculates mean-feature similarity using feats
+  dir_likeli<-function(ob_idx, cat_obs_idx) {
+    cat_feat<-init_feat_dist(beta)
+    for (i in cat_obs_idx) cat_feat<-Map('+', cat_feat, feats[[i]])
+    found<-Map('*', cat_feat, feats[[ob_idx]])
+    return(Reduce('+', found)/Reduce('+', cat_feat))
+  }
   
   # initialize
   states<-list()
@@ -20,33 +28,30 @@ run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_
   print(paste0('Start sampling for ', limit, ' iterations:',
                'of ', cond, ' grouped by ', grouping))
   start_sampler<-Sys.time()
+  
   # Initalization
   state<-rep('c1', nobs)
   func_refs[['c1']]<-sample(hypos$hypo, 1, prob=hypos$prior)
-  
-  # Pre-calculated values
-  join_new<-alpha/(nobs-1+alpha)
-  
-  # # Debugging tests
-  # state=c(paste0('c', seq(5)), 'c2')
-  # for (i in 1:5) {
-  #   f_col<-paste0('post_',i)
-  #   funcs<-hypos[,'hypo'][hypos[,f_col]==max(hypos[,f_col])]
-  #   func_refs[[state[i]]]<-sample(funcs, 1)
-  # }
   # state<-paste0('c', seq(nobs))
   # funcs<-sample(non_empty$hypo, nobs, prob=non_empty$prior, replace=F)
   # for (i in seq(nobs)) func_refs[[state[i]]]<-funcs[i]
-  
+
   n<-1
   while (n<limit) {
-    # sample 1 obs
-    to_update<-sample(seq(nobs), 1) # index
-    obs_to_update<-as.list(task_obs[to_update,])
-    obs_feats<-read_feature(obs_to_update, grouping) 
+    # Choose which one to update
+    probs<-sapply(seq(nobs), function(i) {
+      obs<-which(state==state[i])
+      crp<-if (length(obs)==nobs) 1 else length(obs)/(nobs-1+alpha)
+      catdir<-dir_likeli(i, obs)
+      likeli<-get_likeli(func_refs[[state[i]]], as.list(task_obs[i,]))
+      return(crp*catdir*likeli)
+    })
+    if (Reduce('+', probs)==0) probs<-rep(1, nobs) else probs<-1/exp(probs)
     
-    cat_feat<-Map('+', obs_feats, init_feat_dist(beta))
-    self_resemblance<-sum(unlist(cat_feat)*unlist(obs_feats))/sum(unlist(cat_feat))
+    #to_update<-sample(seq(nobs), 1) # index
+    to_update<-sample(seq(nobs), 1, prob=probs)
+    obs_to_update<-as.list(task_obs[to_update,])
+    self_resemblance<-dir_likeli(to_update, to_update)
     
     # sample new function(s) for this obs from conditional probablity
     post_col<-paste0('post_',to_update)
@@ -70,21 +75,16 @@ run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_
     # Check for existing categories
     propto<-list()
     for (s in unique(state)) {
-      #ob_indx<-other_idx[which(state==s)]
-      ob_indx<-which(state==s)
       # Chinese restaurant process
       join_this<-length(ob_indx)/(nobs-1+alpha)
       # Dirichlet on feature similarity
-      cat_feat<-init_feat_dist(beta)
-      for (i in ob_indx) cat_feat<-Map('+', cat_feat, read_feature(as.list(task_obs[i,]), grouping))
-      resemblance<-sum(unlist(cat_feat)*unlist(obs_feats))/sum(unlist(cat_feat))
-      # Causal power
+      resemblance<-dir_likeli(to_update, which(state==s))
+      # Causal function
       likeli<-get_likeli(func_refs[[s]], obs_to_update)
       # Put together
       propto[[s]]<-join_this*resemblance*likeli
     }
-    # Or assign a new one
-    
+    # Or assign new one(s)
     for (cat in new_cats) {
       if (!(cat %in% state)) {
         post_likeli<-get_likeli(func_refs[[cat]], obs_to_update)
@@ -93,11 +93,11 @@ run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_
     }
     # Sample new category
     state[to_update]<-sample(names(propto), 1, prob=unlist(propto))
-    # ll<-1
-    # for (i in seq(nobs)) ll<-ll*get_likeli(func_refs[[state[i]]], as.list(task_obs[i,]))
-    # print(paste0(n, ': ', paste(state, collapse=','), ' | likeli: ', round(ll,4)))
-    if (logging) print(paste0(n, ': ', 'sampled ', cat, ' for ', to_update,
-                              ' | ', paste(state, collapse=',')))
+    if (logging) print(paste0(n, ': ', 'sampling ', to_update, ' | ', 
+                              paste(state, collapse=',')))
+    # Save everything for developing
+    # Play with burn-in and thinning in the pred.R script
+    # For the final version do built-in burn-in and thinning here
     states[[n]]<-state
     # Go to the next iteration
     n<-n+1
@@ -108,8 +108,9 @@ run_gibbs_sampler<-function(cond, grouping, alpha, beta, limit, logging=T, hypo_
   return(list(state=states, funcs=func_refs))
 }
 
-#x<-run_gibbs_sampler('A1', 'A', 1, .1, 100, T)
-
+x<-run_gibbs_sampler('A1', 'AR', 1, .1, 1000, T)
+df<-data.frame(matrix(unlist(x[[1]]), nrow=length(x[[1]]), byrow=T))
+df<-df%>%group_by(X1, X2, X3, X4, X5, X6)%>%summarise(n=n())%>%ungroup()
 
 
 

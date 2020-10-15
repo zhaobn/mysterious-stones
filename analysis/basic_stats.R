b7f359144ba872cdbe6af5469f1c0de8b4d2a211
bonan <zbn.dale@gmail.com>
2020-10-15 22:52:35 +0100

    Plot ppt selections

--- analysis/basic_stats.R
+++ analysis/basic_stats.R
@@ -4,41 +4,66 @@ options()$scipen
 library(dplyr)
 rm(list=ls())
 
-
 # Print mean ± sd
 ms<-function(vec, time=F) print(paste0(round(mean(vec), 2), ' ± ', round(sd(vec),2)))
 
 # Get filtered data
 df.fs<-df.sw%>%filter(rule_ok>0)
+df.bs<-df.sw%>%filter(rule_ok==0)
 
 # Condition sizes
 df.sw%>%count(condition)
 df.fs%>%count(condition)
 
 # Demographics
-df.sw%>%filter(sex=='female')%>%nrow() # 30/112 = 27%
-df.fs%>%filter(sex=='female')%>%nrow() # 23/72 = 32%
+df.sw%>%filter(sex=='female')%>%nrow() # 48/163 = 29%
+df.fs%>%filter(sex=='female')%>%nrow() # 37/101 = 36%
 
-ms(df.sw$age) # 36.48 ± 10.44
-ms(df.fs$age) # 35.08 ± 9.61
+ms(df.sw$age) # 36.39 ± 10.33
+ms(df.fs$age) # 34.83 ± 9.63
 
 # Task duration
-ms(df.sw$task_duration/60000) # 9.96 ± 5.14
-ms(df.fs$task_duration/60000) # 9.64 ± 4.74
+ms(df.sw$task_duration/60000) # 10.48 ± 6.77
+ms(df.fs$task_duration/60000) # 10.51 ± 7.37
 
 # Mind-change
-sum(df.sw$final_changed)/nrow(df.sw) # 57%
-sum(df.fs$final_changed)/nrow(df.fs) # 42%
+sum(df.sw$final_changed)/nrow(df.sw) # 60%
+sum(df.fs$final_changed)/nrow(df.fs) # 43%
+sum(df.bs$final_changed)/nrow(df.bs) # 89%
 
 # Correction rates
-sum(df.sw$correct)/(nrow(df.sw)*18) # 30%
-sum(df.fs$correct)/(nrow(df.fs)*18) # 43%
+sum(df.sw$correct)/(nrow(df.sw)*18) # 29%
+sum(df.fs$correct)/(nrow(df.fs)*18) # 42%
+sum(df.bs$correct)/(nrow(df.bs)*18) # 7% ~ 5%
+
 df.fs %>%
   group_by(condition) %>%
   summarise(c=sum(correct), n=n()) %>%
   mutate(c_rate = c/(n*18))
 
+# Measure pass-rate of checking trials
+ixes<-df.tw%>%pull(ix)%>%unique()
+df.checks<-data.frame(ix=numeric(0), check_trials=character(0), selection=numeric(0), answer=numeric(0))
+for (i in ixes) {
+  x<-df.tw%>%filter(ix==i & grepl('learn', sid))%>%arrange(sid)
+  gens_sid<-x%>%filter(phase=='gen')%>%pull(sid)
+  correct_answer<-
+    df.checks<-rbind(df.checks, 
+                     data.frame(ix=i, 
+                                check_trials=gens_sid, 
+                                selection=x%>%filter(phase=='gen')%>%pull(result), 
+                                answer=x%>%filter(phase=='learn'&sid%in%gens_sid)%>%pull(result)))
+}
+df.checks<-df.checks%>%
+  left_join(df.tw%>%select(ix, condition)%>%unique(), by='ix')
+df.checks$correct<-as.numeric(df.checks$selection==df.checks$answer)
 
+pass<-df.checks %>%
+  group_by(ix) %>%
+  summarise(correct=sum(correct)) %>%
+  filter(correct==2) %>%
+  pull(ix)
+df.sw<-df.sw%>%mutate(pass=if_else(ix %in% pass, 1, 0))
 
 
 
--- analysis/behav_plots.R
+++ analysis/behav_plots.R
@@ -1,7 +1,9 @@
 
 library(tidyverse)
+load('../data/mturk/mturk_main.Rdata')
 
 badEntries<-df.sw%>%filter(rule_ok==0)%>%pull(id)%>%as.character()
+passed<-df.sw%>%filter(pass>0)%>%pull(id)%>%as.character()
 
 all_results<-c()
 for (e in 3:7) {
@@ -13,64 +15,39 @@ default<-expand.grid(condition=paste0('A', seq(4)), trial=seq(16), result=all_re
 default$condition<-as.character(default$condition)
 
 # Plot data
-data<-df.tw %>%
-  filter(phase=='gen'&grepl('gen', sid)) %>%
-  mutate(trial=as.numeric(substr(sid,8,9))) %>%
-  select(ix, condition, trial, result) %>%
-  arrange(condition, trial) %>%
-  group_by(condition, trial, result) %>%
-  summarise(count=n()) %>%
-  ungroup() %>%
-  right_join(default, by=c('condition', 'trial', 'result')) %>%
-  mutate(count=replace_na(count, 0)) %>%
-  group_by(condition, trial, result) %>%
-  summarise(n = sum(count)) %>%
-  mutate(freq = n / sum(n), result=as.character(result), label='all') %>%
-  mutate(Fixed=if_else(condition %in% c('A1', 'A3'), 'Fixed Agent', 'Fixed Recipient'),
-         Rule=if_else(condition %in% c('A1', 'A2'), 'edge(A)+1, shade(R)+1', 'shade(A)+1, edge(R)+1'))
+prep_plot<-function(data, l) {
+  data<-data %>%
+    filter(phase=='gen'&grepl('gen', sid)) %>%
+    mutate(trial=as.numeric(substr(sid,8,9))) %>%
+    select(ix, condition, trial, result) %>%
+    arrange(condition, trial) %>%
+    group_by(condition, trial, result) %>%
+    summarise(count=n()) %>%
+    ungroup() %>%
+    right_join(default, by=c('condition', 'trial', 'result')) %>%
+    mutate(count=replace_na(count, 0)) %>%
+    group_by(condition, trial, result) %>%
+    summarise(n = sum(count)) %>%
+    mutate(freq = n / sum(n), 
+           result=as.character(result), 
+           label=l,
+           fixed=if_else(condition %in% c('A1', 'A3'), 'Fixed Agent', 'Fixed Recipient'),
+           rule=if_else(condition %in% c('A1', 'A2'), 'edge(A)+1, shade(R)+1', 'shade(A)+1, edge(R)+1')) %>%
+    select(condition, trial, result, prob=freq, label, fixed, rule)
+  return(data)
+}
+all<-prep_plot(df.tw, 'all')
+ok<-prep_plot(filter(df.tw, !(id %in% badEntries)), 'ok')
+bots<-prep_plot(filter(df.tw, id %in% badEntries), 'bot')
+passed<-prep_plot(filter(df.tw, id %in% passed), 'passed')
 
-ggplot(s_data, aes(x=result, y=trial, fill=freq)) + geom_tile() + 
+ggplot(ok, aes(x=result, y=trial, fill=prob)) + geom_tile() + 
   labs(x='object', y='task') +
   scale_y_continuous(trans="reverse", breaks=1:16) + 
   scale_fill_gradient(low='white', high='#293352') +
-  facet_grid(Fixed~Rule)
+  facet_grid(fixed~rule)
 
-# Filtered data
-f_data<-df.tw %>%
-  filter(phase=='gen'&grepl('gen', sid)&!(id %in% badEntries)) %>%
-  mutate(trial=as.numeric(substr(sid,8,9))) %>%
-  select(ix, condition, trial, result) %>%
-  arrange(condition, trial) %>%
-  group_by(condition, trial, result) %>%
-  summarise(count=n()) %>%
-  ungroup() %>%
-  right_join(default, by=c('condition', 'trial', 'result')) %>%
-  mutate(count=replace_na(count, 0)) %>%
-  group_by(condition, trial, result) %>%
-  summarise(n = sum(count)) %>%
-  mutate(freq = n / sum(n), result=as.character(result), label='ok') %>%
-  mutate(Fixed=if_else(condition %in% c('A1', 'A3'), 'Fixed Agent', 'Fixed Recipient'),
-         Rule=if_else(condition %in% c('A1', 'A2'), 'edge(A)+1, shade(R)+1', 'shade(A)+1, edge(R)+1'))
-
-
-b_data<-df.tw %>%
-  filter(phase=='gen'&grepl('gen', sid)&(id %in% badEntries)) %>%
-  mutate(trial=as.numeric(substr(sid,8,9))) %>%
-  select(ix, condition, trial, result) %>%
-  arrange(condition, trial) %>%
-  group_by(condition, trial, result) %>%
-  summarise(count=n()) %>%
-  ungroup() %>%
-  right_join(default, by=c('condition', 'trial', 'result')) %>%
-  mutate(count=replace_na(count, 0)) %>%
-  group_by(condition, trial, result) %>%
-  summarise(n = sum(count)) %>%
-  mutate(freq = n / sum(n), result=as.character(result), label='bad') %>%
-  mutate(Fixed=if_else(condition %in% c('A1', 'A3'), 'Fixed Agent', 'Fixed Recipient'),
-         Rule=if_else(condition %in% c('A1', 'A2'), 'edge(A)+1, shade(R)+1', 'shade(A)+1, edge(R)+1'))
-
-
-ggplot(rbind(f_data, b_data), aes(x=result, y=trial, fill=freq)) + geom_tile() + 
+ggplot(rbind(all, ok, bots, passed), aes(x=result, y=trial, fill=prob)) + geom_tile() + 
   labs(x='object', y='task') +
   scale_y_continuous(trans="reverse", breaks=1:16) + 
   scale_fill_gradient(low='white', high='#293352') +
@@ -93,27 +70,6 @@ ggplot(fc_data,
 t.test(filter(fc_data, condition=='A1')%>%pull(congruency),
        filter(fc_data, condition=='A2')%>%pull(congruency), paired = F)
 
-# measure pass-rate of checking trials
-ixes<-df.tw%>%pull(ix)%>%unique()
-df.checks<-data.frame(ix=numeric(0), check_trials=character(0), selection=numeric(0), answer=numeric(0))
-for (i in ixes) {
-  x<-df.tw%>%filter(ix==i & grepl('learn', sid))%>%arrange(sid)
-  gens_sid<-x%>%filter(phase=='gen')%>%pull(sid)
-  correct_answer<-
-  df.checks<-rbind(df.checks, 
-                   data.frame(ix=i, 
-                              check_trials=gens_sid, 
-                              selection=x%>%filter(phase=='gen')%>%pull(result), 
-                              answer=x%>%filter(phase=='learn'&sid%in%gens_sid)%>%pull(result)))
-}
-df.checks<-df.checks%>%
-  left_join(df.tw%>%select(ix, condition)%>%unique(), by='ix')
-df.checks$correct<-as.numeric(df.checks$selection==df.checks$answer)
-  
-df.checks %>%
-  group_by(condition) %>%
-  summarise(correction=sum(correct), n=n())%>%
-  mutate(correction_rate=correction/n)
 
 # Compare correction rate for gen trials
 df.sw %>%
@@ -124,15 +80,9 @@ df.sw %>%
   mutate(correct_rate=correct/(n*18))
 
 # What if you filter people by getting the check trial correct  
-pass<-df.checks %>%
-  group_by(ix) %>%
-  summarise(correct=sum(correct)) %>%
-  filter(correct==2) %>%
-  pull(ix)
-
 df.sw %>% count(condition)
-df.sw %>% filter(!(id %in% badEntries)) %>% count(condition)
-df.sw %>% filter(ix %in% pass) %>% count(condition)
+df.sw %>% filter(rule_ok==1) %>% count(condition)
+df.sw %>% filter(pass==1) %>% count(condition)
 
 s_data<-df.tw %>%
   filter(phase=='gen' & grepl('gen', sid) & ix %in% pass) %>%

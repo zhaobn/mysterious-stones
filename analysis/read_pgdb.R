ab4cee66d9c5c4e19647a8f56041a580cf5c12af
bonan <zbn.dale@gmail.com>
2020-10-15 22:52:03 +0100

    Save up main experiment data

--- analysis/read_pgdb.R
+++ analysis/read_pgdb.R
@@ -29,8 +29,8 @@ dbExistsTable(con, participantTableName)
 td <- dbGetQuery(con, paste("SELECT * from ", taskTableName))
 
 # Pilot 
-pilot_start = 131
-pilot_end = 174
+pilot_start = 175
+pilot_end = 183
 
 subject <- td$subject[c(pilot_start:pilot_end)]
 trials <- td$trials[c(pilot_start:pilot_end)]
@@ -71,7 +71,7 @@ for (i in 2:nsubs) {
   sj = as.data.frame(inv_fromJSON(subject[i], TRUE))
   df.sw.aux = rbind(df.sw.aux, sj)
 }
-write.csv(df.sw.aux, file='main_3.csv')
+write.csv(df.sw.aux, file='main_4.csv')
 
 
 df.tw.aux = (as.data.frame(inv_fromJSON(trials[1], FALSE)))
@@ -101,8 +101,9 @@ df.sw$task_duration<-as.numeric(as.character(df.sw$task_duration))
 df.sw$initial_certainty<-as.numeric(as.character(df.sw$initial_certainty))
 df.sw$final_changed<-as.numeric(as.character(df.sw$final_changed))
 df.tw$condition<-as.character(df.tw$condition)
+df.tw$sid<-as.character(df.tw$sid)
 ## Save data
-save(df.sw, df.tw, file='../data/mturk/main_batches/mturk_20201013.Rdata') 
+save(df.sw, df.tw, file='../data/mturk/main_batches/mturk_20201014.Rdata') 
 
 # Combine data
 b.sw<-df.sw
@@ -115,15 +116,27 @@ save(df.sw, df.tw, file='../data/mturk/mturk_main.Rdata')
 # Clean up duplicated lines
 df.sw <- df.sw %>% arrange(ix)
 rownames(df.sw)<-NULL
+df.sw<-df.sw[-c(112:113),]
 
 rownames(df.tw)<-NULL
-test<-df.tw[-c(1873:1920),]
+test<-df.tw[-c(2689:2736),]
 df.tw<-test
 
 # Add rule_ok labels
-labels<-read.csv('label.csv')
+labels<-read.csv('labels.csv')
 df.sw<-select(df.sw,-'rule_ok')
 df.sw<-df.sw%>%left_join(labels, by='token')
 
-
+# Read task setups
+tasks <-
+  df.tw %>%
+  filter(phase=='learn' | grepl('gen', sid)) %>%
+  select(phase, sid, agent, recipient, result, condition) %>%
+  mutate(result=if_else(phase=='gen', 0, result), 
+         task=as.numeric(substr(sid, nchar(sid)-1, nchar(sid)))) %>%
+  group_by(condition, phase, agent, recipient, result, task, sid) %>%
+  summarise(n=n()) %>%
+  ungroup() %>%
+  select(condition, phase, task, agent, recipient, result, sid)
+write_csv(tasks, '../data/setup/main.csv')
 
